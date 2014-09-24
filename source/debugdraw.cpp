c614fd8c99450ebffa305783cad3a63933320c81
Nicholas Flynt <thegamefreak0134@gmail.com>
2014-09-24 14:38:56 -0500

    Make it green! And checkered! (ground plane)

--- source/debugdraw.cpp
+++ source/debugdraw.cpp
@@ -22,12 +22,16 @@ void debug::drawCrosshair(Vec3 p, rgb color) {
 void debug::drawGroundPlane(int width, int segments, rgb color) {
     glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE | (1<<12));
     glBegin(GL_TRIANGLE);
-    glColor(color);
     glPushMatrix();
     glScalef(width / segments, 0, width / segments);
     for (int z = 0; z < segments; z++) {
         for (int x = 0; x < segments; x++) {
-            //glColor(rand());
+            if ((z + x) % 2 == 0) {
+                glColor(color);
+            } else {
+                //glColor(color & 0xBDEF); //halve the color value
+                glColor(color & 0xDEF7); //approximately quarter the color value
+            }
             glVertex3f(-1.0f + (2.0f / segments) *  x     , 0,  -1.0f + (2.0f / segments) *  z);
             glVertex3f(-1.0f + (2.0f / segments) * (x + 1), 0,  -1.0f + (2.0f / segments) *  z);
             glVertex3f(-1.0f + (2.0f / segments) * (x + 1), 0,  -1.0f + (2.0f / segments) * (z + 1));
