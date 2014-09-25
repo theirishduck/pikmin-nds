bcb1ad8214144a26d119097eee0a438c8c223c1b
Cristián Romo <cristian.g.romo@gmail.com>
2014-09-25 00:14:24 -0500

    Added Brads
    Another type for use in the typesafe libnds wrappers, yay!

--- include/NumericTypes.h
+++ include/NumericTypes.h
@@ -15,8 +15,8 @@ class Degrees
 public:
   using value_type = s32;
 
-  Degrees() : data{0} {}
-  Degrees(Degrees const& other) : data{other.data} {}
+  constexpr Degrees() : data{0} {}
+  constexpr Degrees(Degrees const& other) : data{other.data} {}
   explicit constexpr Degrees(value_type const value) : data{value} {}
   Degrees& operator=(Degrees const other) {data = other.data; return *this;}
 
@@ -38,8 +38,8 @@ public:
   Degrees const operator%(Degrees const other) const {return Degrees{mod32(data, other.data)};}
   Degrees& operator%=(Degrees const other) {data = mod32(data, other.data); return *this;}
 
-  constexpr Degrees div(Degrees const other) const {return Degrees{data / other.data};}
-  constexpr Degrees mod(Degrees const other) const {return Degrees{data % other.data};}
+  constexpr Degrees Div(Degrees const other) const {return Degrees{data / other.data};}
+  constexpr Degrees Mod(Degrees const other) const {return Degrees{data % other.data};}
 
   value_type data;
 };
@@ -48,7 +48,54 @@ namespace literals {
 constexpr Degrees operator"" _deg(unsigned long long value) {
   return Degrees{static_cast<Degrees::value_type>(value)};
 }
-}  // namespace user_defined_literals
+}  // namespace literals
+
+class Brads
+{
+public:
+  using value_type = s16;
+
+  constexpr Brads() : data{0} {}
+  constexpr Brads(Brads const& other) : data{other.data} {}
+  static constexpr Brads Raw(value_type const value) {return Brads{value};}
+  Brads& operator=(Brads const other) {data = other.data; return *this;}
+
+  constexpr bool operator==(Brads const other) const {return data == other.data;}
+  constexpr bool operator<(Brads const other) const {return data < other.data;}
+  constexpr bool operator>(Brads const other) const {return data > other.data;}
+  constexpr bool operator<=(Brads const other) const {return data <= other.data;}
+  constexpr bool operator>=(Brads const other) const {return data >= other.data;}
+  constexpr bool operator!=(Brads const other) const {return data != other.data;}
+
+  constexpr Brads operator+(Brads const other) const {return Brads{static_cast<value_type>(data + other.data)};}
+  Brads& operator+=(Brads const other) {data += other.data; return *this;}
+  constexpr Brads operator-(Brads const other) const {return Brads{static_cast<value_type>(data - other.data)};}
+  Brads& operator-=(Brads const other) {data -= other.data; return *this;}
+  constexpr Brads operator*(Brads const other) const {return Brads{static_cast<value_type>(data * other.data)};}
+  Brads& operator*=(Brads const other) {data *= other.data; return *this;}
+  Brads const operator/(Brads const other) const {return Brads{static_cast<value_type>(div32(data, other.data))};}
+  Brads& operator/=(Brads const other) {data = static_cast<value_type>(div32(data, other.data)); return *this;}
+  Brads const operator%(Brads const other) const {return Brads{static_cast<value_type>(mod32(data, other.data))};}
+  Brads& operator%=(Brads const other) {data = static_cast<value_type>(mod32(data, other.data)); return *this;}
+
+  constexpr Brads Div(Brads const other) const {return Brads{static_cast<value_type>(data / other.data)};}
+  constexpr Brads Mod(Brads const other) const {return Brads{static_cast<value_type>(data % other.data)};}
+
+  value_type data;
+
+private:
+  explicit constexpr Brads(value_type const value) : data{value} {}
+};
+
+namespace literals {
+constexpr Brads operator"" _brad(long double value) {
+  return Brads::Raw(static_cast<Brads::value_type>(degreesToAngle(value)));
+}
+
+constexpr Brads operator"" _brad(unsigned long long value) {
+  return Brads::Raw(static_cast<Brads::value_type>(degreesToAngle(value)));
+}
+}  // namespace literals
 
 }  // namespace numeric_types
 
