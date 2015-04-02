c2c5dbf43cd076d5304b46ebf34c11df0017227c
Nicholas Flynt <thegamefreak0134@gmail.com>
2015-04-01 23:38:46 -0500

    Tweak the camera parameters for correctness
    Getting the feel just right is tricky on such a small screen

--- arm9/source/entities/camera.cpp
+++ arm9/source/entities/camera.cpp
@@ -15,82 +15,66 @@ using numeric_types::literals::operator"" _brad;
 using numeric_types::Brads;
 
 Camera::Camera() {
-  position_destination_ = Vec3{0_f, 6_f, 4_f};
-  target_destination_  = Vec3{0_f, 3_f, 0_f};
-
-  position_current_ = position_destination_;
-  target_current_ = target_destination_;
-
   SetCache();
 }
 
 void Camera::Update() {
   if (keysDown() & KEY_R) {
     if (keysHeld() & KEY_L) {
-      distance_ += 1;
-      if (distance_ > 3) {
-        distance_ = 1;
+      target_state_.distance += 7_f;
+      if (target_state_.distance > 24_f) {
+        target_state_.distance = 10_f;
       }
     } else {
-      high_camera_ = not high_camera_;
+      if (target_state_.height == 0.5_f) {
+        target_state_.height = 2_f;
+      } else {
+        target_state_.height = 0.5_f;
+      }
     }
   }
 
   if (entity_to_follow_) {
-    fixed const height = (high_camera_ ? 7.0_f : 3.0_f) *
-        fixed::FromInt(distance_ + 1);
-
     if (keysDown() & KEY_L) {
       // Move the camera directly behind the target entity, based on their
       // current rotation.
-      position_destination_ = entity_to_follow_->position();
-      position_destination_.x.data_ -=
-          cosLerp((entity_to_follow_->rotation().y - 90_brad).data_);
-      position_destination_.z.data_ -=
-          -sinLerp((entity_to_follow_->rotation().y - 90_brad).data_);
+      target_state_.angle = entity_to_follow_->rotation().y - 90_brad;
     }
-
-    fixed follow_distance = 4.0_f + 6.0_f * fixed::FromInt(distance_)
-        + (high_camera_ ? 0_f : 1_f);
-
-    target_destination_ = entity_to_follow_->position();
-    Vec3 entity_to_camera = entity_to_follow_->position() -
-        position_destination_;
-    entity_to_camera.y = 0_f;  // Clear out height to work in the XZ plane.
-    entity_to_camera = entity_to_camera.Normalize();
-    entity_to_camera = entity_to_camera * follow_distance;
-    position_destination_ = entity_to_follow_->position() - entity_to_camera;
-    position_destination_.y = entity_to_follow_->position().y + height;
-
-    /*printf("\x1b[8;0HC. Position: %.1f, %.1f, %.1f\n",
-        (float)position_destination_.x, (float)position_destination_.y,
-        (float)position_destination_.z);
-    printf("C. Target  : %.1f, %.1f, %.1f\n", (float)target_destination_.x,
-        (float)target_destination_.y, (float)target_destination_.z);
-    */
-    debug::DisplayValue("C.Position", position_destination_);
-    debug::DisplayValue("C.Target", target_destination_);
+    target_state_.target = entity_to_follow_->position() + Vec3{0_f,2.5_f,0_f};
   } else {
     //printf("No entity?\n");
   }
 
   // Take the weighted average for position and target to take a smooth
   // transition between the old value and the new one.
-  position_current_ = position_destination_ * 0.25_f + position_current_ *
-      0.75_f;
-  target_current_ = target_destination_ * 0.25_f + target_current_ * 0.75_f;
+  
+  current_state_.target = current_state_.target * 0.875_f
+      + target_state_.target * 0.125_f;
+
+  current_state_.height = current_state_.height * 0.875_f
+      + target_state_.height * 0.125_f;
+
+  current_state_.distance = current_state_.distance * 0.875_f
+      + target_state_.distance * 0.125_f;
+
+  // For the angle, we need to do fancy delta clamping
+  //TODO: that later
+  current_state_.angle = target_state_.angle;
+
 }
 
 void Camera::LookAt(Vec3 position, Vec3 target, bool instant) {
+  /*
   position_destination_ = position;
   target_destination_ = target;
   if (instant) {
     position_current_ = position;
     target_current_ = target;
-  }
+  }*/
 }
 
 Brads Camera::GetAngle() {
+  /*
   Vec3 facing;
   facing = entity_to_follow_->position() - position_current_;
   facing.y = 0_f;  // Work on the XZ plane.
@@ -104,7 +88,8 @@ Brads Camera::GetAngle() {
     return Brads::Raw(acosLerp(facing.x.data_));
   } else {
     return Brads::Raw(-acosLerp(facing.x.data_));
-  }
+  }*/
+  return current_state_.angle;
 }
 
 void Camera::FollowEntity(DrawableEntity* entity) {
@@ -113,13 +98,25 @@ void Camera::FollowEntity(DrawableEntity* entity) {
 
 // TODO(Nick) Convert this to non-float implementation.
 void Camera::ApplyTransform() {
+  // With our camera parameters, generate a position and a target for
+  // OpenGL's LookAt function
+
+  Vec3 offset{0_f,0_f,0_f};
+  offset.x.data_ = -cosLerp(cached_state_.angle.data_);
+  offset.z.data_ = sinLerp(cached_state_.angle.data_);
+  offset.y = cached_state_.height;
+  offset = offset.Normalize();
+  offset *= cached_state_.distance + cached_state_.height;
+
+  Vec3 position = cached_state_.target + offset;
+  Vec3 target = cached_state_.target;
+
   gluLookAt(
-      (float)position_cached_.x, (float)position_cached_.y,
-      (float)position_cached_.z, (float)target_cached_.x,
-      (float)target_cached_.y, (float)target_cached_.z, 0.0f, 1.0f, 0.0f);
+      (float)position.x, (float)position.y,
+      (float)position.z, (float)target.x,
+      (float)target.y, (float)target.z, 0.0f, 1.0f, 0.0f);
 }
 
 void Camera::SetCache() {
-  position_cached_ = position_current_;
-  target_cached_ = target_current_;
+  cached_state_ = current_state_;
 }
