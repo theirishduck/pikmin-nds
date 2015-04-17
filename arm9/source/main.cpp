826b0e65030266373d0c04e3b6c9491465768b3e
Nicholas Flynt <thegamefreak0134@gmail.com>
2015-04-16 22:25:05 -0500

    Apply low-poly treatment to blue and yellow pikmin

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -131,8 +131,8 @@ void SetupDemoPikmin() {
       //*
       PikminState* pikmin = g_game.SpawnObject<PikminState>();
       //random colors!
-      //pikmin->type = (PikminType)(rand() % 3);
-      pikmin->type = PikminType::kRedPikmin;
+      pikmin->type = (PikminType)(rand() % 3);
+      //pikmin->type = PikminType::kRedPikmin;
       pikmin->entity->body()->position = {
         fixed::FromInt(-10 + j * 2 + 64),
         0_f,
