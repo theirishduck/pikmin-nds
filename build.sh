1347a95069470f6054d94be5144a5ce73a38b76c
Nicholas Flynt <thegamefreak0134@gmail.com>
2015-05-10 18:18:39 -0500

    Add a one-shot build script
    This *should* work

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# build the docker file if it isn't already here
+docker build --quiet=true -t zeta0134/pikmin-nds ./docker
+
+# run the dockerfile on this directory, which will build the project
+docker run -it --rm -v $(pwd):/source zeta0134/pikmin-nds
