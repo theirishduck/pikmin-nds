b9cc8ba2296ac6454d273bf4c4fcb7089ede6401
Nicholas Flynt <thegamefreak0134@gmail.com>
2015-05-10 19:51:41 -0500

    Don't use the -t flag
    Fixes an issue with piping the output of make, which was interfering
    with Sublime.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 docker build --quiet=true -t zeta0134/pikmin-nds ./docker
 
 # run the dockerfile on this directory, which will build the project
-docker run -it --rm -v $(pwd):/source zeta0134/pikmin-nds
+docker run -i --rm -v $(pwd):/source zeta0134/pikmin-nds
