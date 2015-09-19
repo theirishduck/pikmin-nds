974dd701fec7bffa83bd7c90416084c9be6b2927
Cristián Romo <cristian.g.romo@gmail.com>
2015-09-19 10:53:10 -0500

    Make textures use relative paths.
    The default path mode that blender uses for exporting .fbx files is
    AUTO, which tries to be smart and decide whether to use absolute or
    relative paths in the conversion. In this case, it was guessing
    incorrectly. To fix it, tell the exporter to always use relative paths.

--- tools/export_fbx.py
+++ tools/export_fbx.py
@@ -6,4 +6,5 @@ for object in bpy.data.objects:
 bpy.data.objects['red_pikmin'].select = True
 bpy.data.objects['Armature'].select = True
 
-bpy.ops.export_scene.fbx(filepath='red_pikmin.fbx', use_selection=True, add_leaf_bones=False)
+bpy.ops.export_scene.fbx(filepath='red_pikmin.fbx', use_selection=True,
+    add_leaf_bones=False, path_mode='RELATIVE')
