2a14bdd048cc5488790d5494c4462ba8ebd4054b
Cristián Romo <cristian.g.romo@gmail.com>
2015-09-19 00:18:24 -0500

    Add beginning of blender to .fbx export script.
    The only part of asset conversion that is manual at the moment is
    exporting to .fbx files from within Blender. This script aims to
    eventually make this step automatic, so that the entire asset pipeline
    is automatic and can be run as part of the build.
    
    Eventually, this may import the dsgx converter as a library and
    synthesize the .dsgx file directly from a .blend file, removing our
    reliance on the FBX SDK and improving build times.

---
+++ tools/export_fbx.py
@@ -0,0 +1,9 @@
+import bpy
+
+for object in bpy.data.objects:
+    object.select = False
+
+bpy.data.objects['red_pikmin'].select = True
+bpy.data.objects['Armature'].select = True
+
+bpy.ops.export_scene.fbx(filepath='red_pikmin.fbx', use_selection=True, add_leaf_bones=False)
