8dc7e775e749aa2d4953b34f9a793eedb1dfeb1b
= <thegamefreak0134@gmail.com>
2015-11-22 10:17:44 -0600

    Pillow requires libjpeg-dev in newer versions

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # build the docker image if necessary
-docker build --quiet=true -t zeta0134/pikmin-nds ./docker
+docker build -t zeta0134/pikmin-nds ./docker
 
 # run the dockerfile on this directory, which will build the project
 docker run -i --rm -v $(pwd):/source zeta0134/pikmin-nds
