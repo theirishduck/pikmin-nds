f9f0e26986b16b042357a3b7797e14078a18f802
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-12-09 17:01:30 -0600

    Refer to the current directory properly on Windows

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 :: build the docker image if necessary
-docker build --build-arg external_username=docker --build-arg external_uid=1000 --build-arg external_gid=1000 -t pikmin-nds .
+docker build --build-arg external_username=docker --build-arg external_uid=1000 --build-arg external_gid=1000 -t pikmin-nds %cd%
 
 :: run the dockerfile on this directory, which will build the project
 docker run -i --rm -v "%cd%":/source pikmin-nds
