77070d9be9c8eeb8a1b65d801b9b8c4c1ed85526
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-12-31 01:20:15 -0600

    Pass in -t on Windows
    This allows running "build.bat bash" and other interactive variants. Now has parity with bash.sh.

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 docker build --build-arg external_username=docker --build-arg external_uid=1000 --build-arg external_gid=1000 -t pikmin-nds %cd%
 
 :: run the dockerfile on this directory, which will build the project
-docker run -i --rm -v "%cd%":/source pikmin-nds %*
+docker run -i -t --rm -v "%cd%":/source pikmin-nds %*
